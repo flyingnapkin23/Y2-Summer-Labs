2a0511ee4f62f491c803c42a10a7505b8fbdec93
Tuts <tulineAYYSH@GMAIL.COM>
2024-07-15 10:00:58 +0300

    updated Pyhton Review 2.1

--- 2.1 python review #1/2.1PythRev.py
+++ 2.1 python review #1/2.1PythRev.py
@@ -1,3 +1,4 @@
+import random
 tempatures = random.sample(range(26, 40), 7)
 days_of_week = ["Sunday","Monday","Tuesday","Wednsday","Thursday","Friday","Saturday"]
 good_days_count = 0
@@ -6,7 +7,7 @@ min = 40
 max_day = "Saturday"
 min_day = "Saturday"
 all_temp = 0
-for i in range len(tempatures):
+for i in range (len(tempatures)):
 
 	if tempatures[i] > max:
 		max = tempatures[i]
@@ -27,28 +28,28 @@ average = all_temp / 7
 average_list = []
 average_list_day = []
 
-for i in range len(tempatures):
+for i in range (len(tempatures)):
 	if tempatures[i]> average:
 		average_list.append(tempatures[i])
-		average_list.append(days_of_week[i])
+		average_list_day.append(days_of_week[i])
 
 
-for i in range len(tempatures):
-	print("On " + days_of_week[i] + " the tempaerature is " + tempatures[i])
+for i in range (len(tempatures)):
+	print("On ", days_of_week[i], " the tempaerature is ", tempatures[i])
 
 	if tempatures[i] % 2 == 0:
 		good_days_count += 1
 		print (days_of_week[i]," is a good day!")
-print (max_day + " is the day with the highest temperature of " + max)
-print (min_day + " is the day with the lowest temperature of " + min)
-print (average + " is the average temperature for this week!")
+print (max_day, " is the day with the highest temperature of ", max)
+print (min_day, " is the day with the lowest temperature of ", min)
+print (average, " is the average temperature for this week!")
 
-for i in range len(average_list):
-	print (average_list_day[i] + " is a day with and above average temperature with the temperature of " + average_list[i])
+for i in range (len(average_list)):
+	print (average_list_day[i], " is a day with and above average temperature with the temperature of ", average_list[i])
 
 def min_find_remove (temps):
 	min = 40
-	for i in len(temps):
+	for i in range (len(temps)):
 		if temps[i] < min:
 			min = temps[i]
 	temps.remove(min)
@@ -59,7 +60,7 @@ sorted_temp = []
 
 tempatures_copy = tempatures
 
-for i in len(tempatures_copy):
+for i in range (len(tempatures_copy)):
 	sorted_temp.append(min_find_remove(tempatures_copy))
-for i in len(sorted_temp):
-	print(sorted_temp[i] + ", ")
+for i in range (len(sorted_temp)):
+	print(sorted_temp[i], ", ")
